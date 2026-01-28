c9fa3ef7816adf4c1d41e8e5564c127e43b4510b
Om Prakash Pant <ompant229@gmail.com>
2026-01-28 18:24:15 +0545

    Frontend part updated with about and contact section

--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html',{})
+    products = models.Product.objects.all()
+    return render(request, 'home.html',{'products': products})
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
